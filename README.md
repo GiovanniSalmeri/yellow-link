088f98d30b767ca1deaf0f17e20040797c84caaf
Giovanni <g.salmeri@mondodomani.org>
2022-08-28 18:33:35 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# yellow-link
-Internal and external links
+Link extension 0.8.20
+=====================
+Internal and external links.
+
+<p align="center"><img src="link-screenshot.png?raw=true" width="795" height="836" alt="Screenshot"></p>
+
+## How to create an internal link
+
+Create a `[link]` shortcut. 
+
+The following arguments are available, the second is optional:
+
+`target` = the slug (possibly with the #fragment) of the internal page in `content`, or the name of the file in `media/downloads`, or the address of the external page or file  
+`text` = the text to be used in the link (if omitted, for internal pages the title of the page is used), wrap multiple words into quotes  
+
+Internal links work no matter what is the subdirectory where the page or the file is located. When the target is a file to be downloaded, type and size are automatically shown. Size of remote files is cached in file `system/link.csv`. Dead links are detected.
+
+## Examples
+
+Creating an internal link:
+
+```
+[link somepage]  
+[link somepage#fragment]  
+[link somepage "A beautiful page"]  
+[link somefile.pdf "An interesting reading"]
+```
+
+Creating an internal link to different pages with the same slug or to different files with the same name:
+
+```
+[link path/to/somepage]  
+[link anotherpath/to/somepage]  
+[link path/to/somefile]  
+[link anotherpath/to/somefile]  
+```
+
+Creating an external link:
+
+```
+[link https://example.com/somepage "An example page"]  
+[link https://example.com/somepage.html "Another example page"]  
+[link https://example.com/somefile.pdf "A PDF file"]  
+```
+
+## Settings
+
+The following settings can be configured in file `system/extensions/yellow-system.ini`:
+
+`LinkCacheLifeSpan` (default: `30`) = lifespan (in days) of the cache of size and availability of external pages and files  
+`LinkRemoteFilesTimeout` (default: `4`) = maximum time (in seconds) allowed to get informations form external pages and files  
+
+## Installation
+
+[Download extension](https://github.com/GiovanniSalmeri/yellow-link /archive/master.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+## Developer
+
+Giovanni Salmeri. [Get help](https://github.com/GiovanniSalmeri/yellow-link/issues).
---
+++ link.php
@@ -0,0 +1,155 @@
+<?php
+// Link extension, https://github.com/GiovanniSalmeri/yellow-flink
+
+class YellowLink {
+    const VERSION = "0.8.20";
+    public $yellow;         //access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+        $this->yellow->system->setDefault("linkCacheLifeSpan", "30");
+        $this->yellow->system->setDefault("linkRemoteFilesTimeout", "4");
+    }
+    
+    // Handle page content of shortcut
+    public function onParseContentShortcut($page, $name, $text, $type) {
+        $output = null;
+        if ($name=="link" && $type=="inline") {
+            list($target, $label) = $this->yellow->toolbox->getTextArguments($text);
+            if (preg_match("/^\w+:/", $target)) { // is external
+                if (empty($label)) $label = $target;
+                $path = parse_url($target, PHP_URL_PATH);
+                $fileSize = $this->remoteSize($target);
+                if (preg_match('/\.(\w+)$/', $path, $matches) && !in_array($matches[1], [ "html", "htm", "txt" ])) { // is a download (very naive)
+                    $fileType = strtolower($matches[1]);
+                    $output = $this->makeLink($target, $label, $fileSize==-2, $fileType, $fileSize==-2 ? null : $fileSize);
+                } else { // is not a download
+                    $output = $this->makeLink($target, $label, $fileSize==-2);
+                }
+            } else { // is internal
+                if (empty($label)) $label = $target;
+                if ($target[0]!=="/") $target = "/".$target;
+                if (preg_match('/\.(\w+)$/', $target, $matches)) { // is a download
+                    $path = $this->yellow->lookup->findMediaDirectory("coreDownloadLocation");
+                    $fileNames = $this->yellow->toolbox->getDirectoryEntriesRecursive($path, "/\\./", false, false, 10);
+                    $found = false;
+                    foreach ($fileNames as $fileName) {
+                        if (substr($fileName, -strlen($target))==$target) {
+                            $found = true;
+                            break;
+                        }
+                    }
+                    if ($found) {
+                        $fileType = strtolower($matches[1]);
+                        $fileSize = filesize($fileName);
+                        $fileName = substr($fileName, strlen($path));
+                        $location = $this->yellow->system->get("CoreDownloadLocation").$fileName;
+                        $output = $this->makeLink($location, $label, false, $fileType, $fileSize);
+                    } else {
+                        $location = $this->yellow->system->get("CoreDownloadLocation").substr($target, 1);
+                        $output = $this->makeLink($location, $label, true);
+                    }
+                } else { // is not a download
+                    list($slug, $fragment) = $this->yellow->toolbox->getTextList($target, "#", 2);
+                    $found = false;
+                    foreach ($this->yellow->content->index(true) as $page) {
+                        $location = $page->getLocation(true);
+                        if (substr(rtrim($location, "/"), -strlen($slug))==$slug) {
+                            if (empty($label)) $label = $page->get("title");
+                            $found = true;
+                            break;
+                        }
+                    }
+                    if ($found) {
+                        $output = $this->makeLink($location.($fragment ? "#$fragment" : ""), $label);
+                    } else {
+                        $slug = substr($slug, 1);
+                        if (empty($label)) $label = $slug;
+                        $output = $this->makeLink($slug, $slug, true);
+                    }
+                }
+            }
+        }
+        return $output;
+    }
+
+    // Check for existence and get size of remote files
+    private function remoteSize($address) {
+        $cache = [];
+        // format: address,filesize,timestamp
+        // the cache can be manually edited; a timestamp 0 prevents updating
+        $fileName = $this->yellow->system->get("coreExtensionDirectory")."link.csv";
+        $fileHandle = @fopen($fileName, "r");
+        if ($fileHandle) {
+            while ($data = fgetcsv($fileHandle)) {
+                $cache[$data[0]] = [ $data[1], $data[2] ];
+            }
+            fclose($fileHandle);
+        }
+        $cacheLifeSpan = $this->yellow->system->get("linkCacheLifeSpan");
+        if (!isset($cache[$address]) || $cache[$address][1]!==0 && $cache[$address][1]+$cacheLifeSpan*86400<time()) {
+            $remoteFilesTimeout = $this->yellow->system->get("linkRemoteFilesTimeout");
+            $curl = curl_init();
+            curl_setopt_array($curl, [
+                CURLOPT_URL=>$address,
+                CURLOPT_SSL_VERIFYPEER=>false, // speed up
+                CURLOPT_USERAGENT=>$_SERVER['HTTP_USER_AGENT'], // for paranoid servers
+                CURLOPT_FOLLOWLOCATION=>true,
+                CURLOPT_RETURNTRANSFER=>true,
+                CURLOPT_HEADER=>true,
+                CURLOPT_NOBODY=>true,
+                CURLOPT_TIMEOUT=>$remoteFilesTimeout,
+            ]);
+            curl_exec($curl);
+            if (curl_getinfo($curl, CURLINFO_RESPONSE_CODE)!==200) {
+                $fileSize = -2;
+            } else {
+                $fileSize = curl_getinfo($curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+            }
+            $cache[$address] = [ $fileSize, time() ];
+            $fileHandle = @fopen($fileName, "w");
+            if ($fileHandle) {
+                if (flock($fileHandle, LOCK_EX)) {
+                    foreach ($cache as $key=>$value) {
+                        fputcsv($fileHandle, [ $key, $value[0], $value[1] ]);
+                    }
+                    flock($fileHandle, LOCK_UN);
+                }
+                fclose($fileHandle);
+            } else {
+                $this->yellow->log("error", "Can't write file '$fileName'!");
+            }
+        }
+        return (int)$cache[$address][0];
+    }
+
+    // Make the link
+    private function makeLink($link, $label, $missing = false, $fileType = null, $fileSize = null) {
+        $output = "<a".($missing ? " class=\"link-missing\"" : "")." href=\"".htmlspecialchars($link)."\">".htmlspecialchars($label);
+        if ($fileType) {
+            $output .= " (<span class=\"link-filetype\">".htmlspecialchars($fileType)."</span>".($fileSize ? " <span class=\"link-filesize>".$this->readableSize($fileSize)."</span>" : "").")";
+        }
+        $output .= $missing ? " [".htmlspecialchars($this->yellow->language->getText("linkDeadLink"))."]" : "";
+        $output .= "</a>";
+        return $output;
+    }
+
+    // Human readable sizes
+    private function readableSize($bytes) {
+        $digitalUnit = $this->yellow->language->getText("linkDigitalUnit");
+        $prefixes = [ "", "k", "M", "G", "T", "P" ];
+        for ($i = 0; $bytes>=1000 && $i<(count($prefixes) - 1); $i++) $bytes /= 1000;
+        return str_replace(".", $this->yellow->language->getText("coreDecimalSeparator"), round($bytes, 1))."\u{A0}".$prefixes[$i].$digitalUnit;
+    }
+
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name=="header") {
+            $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
+            $output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}link.css\" />\n";
+        }
+        return $output;
+    }
+}
